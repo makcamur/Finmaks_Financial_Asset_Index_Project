f459692c214c9ea181db4aedda4d1680ef0b6a5c
mertcan akcamur <akcamurmertcan@gmail.com>
2023-09-16 15:15:23 +0300

    Import Api Controller

---
+++ Finmaks_Financial_Asset_Index_Project.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Finmaks_Financial_Asset_Index_Project.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
