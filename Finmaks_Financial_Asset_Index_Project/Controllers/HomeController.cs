509aa663796266021883cae50fc449aafc109228
mertcan akcamur <akcamurmertcan@gmail.com>
2023-09-16 15:11:48 +0300

    Add project files.

---
+++ Finmaks_Financial_Asset_Index_Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Finmaks_Financial_Asset_Index_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Finmaks_Financial_Asset_Index_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Finmaks_Financial_Asset_Index_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Finmaks_Financial_Asset_Index_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
